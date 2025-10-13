ac3e431103ec1e270860da4cbcfd7511b749e605
osaaxe-1 <oscar.axelstrom@hotmail.se>
2025-10-13 15:55:50 +0200

    print

--- internal/node/nodelookup.go
+++ internal/node/nodelookup.go
@@ -40,6 +40,7 @@ func (n *Node) nodeLookup(key string, findValue ...bool) ([]Triple, []byte, bool
 	queried := make(map[string]bool)
 
 	for {
+		fmt.Printf("Shortlist: %s\n", shortlist)
 		toQuery := make([]Triple, 0, Alpha) // Select up to Alpha unqueried nodes
 		for _, contact := range shortlist {
 			if !queried[contact.Addr.String()] && len(toQuery) < Alpha {
